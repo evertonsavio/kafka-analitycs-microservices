7ea8140da728c6be298318badd3c088459162e67
havyx <everluca@outlook.com>
2021-04-17 16:30:45 -0300

    :sparkles: welcome to microservices with twitter and kafka

---
+++ twitter-to-kafka-service/src/main/java/dev/evertonsavio/twittertokafkaservice/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,32 @@
+package dev.evertonsavio.twittertokafkaservice;
+
+import dev.evertonsavio.twittertokafkaservice.config.TwitterToKafkaServiceConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    public static final Logger LOG =  LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+
+    public TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
+        LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[]{})));
+    }
+}
