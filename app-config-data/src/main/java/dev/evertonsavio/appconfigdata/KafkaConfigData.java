d270f30f3abcc85655f3ab80bf3ac123da667354
havyx <everluca@outlook.com>
2021-04-19 20:24:20 -0300

    :sparkles: configurations classes

---
+++ app-config-data/src/main/java/dev/evertonsavio/appconfigdata/KafkaConfigData.java
@@ -0,0 +1,14 @@
+package dev.evertonsavio.appconfigdata;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "kafka-config")
+public class KafkaConfigData {
+
+    
+
+}
